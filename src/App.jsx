2c162701944339a3d296afa2d24f2b7b99b342a9
Abhishek Awasthi <avasthiabhishek0033@gmail.com>
2021-06-29 00:43:15 +0530

    map not working commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
-import React,{useEffect} from 'react';
-import {Button} from '@material-ui/core';
+import React,{useEffect,useState} from 'react';
 import Navbar from "./components/Navbar";
 import MyCard from "./components/MyCard";
-import {getUser} from "./api/Api.jsx";
+import {getMatches} from "./api/Api.jsx";
+//import { Container, Grid } from "@material-ui/core";
 
 
 const App =()=>{
 
+    const [matches,setMatches]=useState([]);
+
     useEffect(()=>{
-        getUser();
+        getMatches()
+        .then((data)=>setMatches(data.matches))
+        .catch((error)=>alert('SORRY'));
     },[]);
-    
+
     return(
         <>
         <div className="App">
             <Navbar/>
             <h1 className="text-center">Welcum to Live Score App</h1>
-            <Button variant="contained" color="secondary">Click Here</Button>
-            <MyCard/>
-
-            {}
+            {/* {matches.map((match)=>(
+                <MyCard match="match"/>
+            ))} */}
         </div>
         </>
     )
