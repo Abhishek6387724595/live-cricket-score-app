6e600a4b281f17490599364b4cf3bdead90bf358
Abhishek Awasthi <avasthiabhishek0033@gmail.com>
2021-06-30 13:32:53 +0530

    For lapton Done yet doing for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React,{useEffect,useState} from 'react';
 import Navbar from "./components/Navbar";
 import MyCard from "./components/MyCard";
 import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
+import { Typography } from '@material-ui/core';
 //import {getMatches} from "./api/Api.jsx";
 //import { Container, Grid } from "@material-ui/core";
 const API_KEY="SZ5pcaz1Zrdel8jtcTBhEdq9sKz2";
@@ -21,12 +23,13 @@ function App() {
             const resp=await fetch(`https://cricapi.com/api/matches?apikey=${API_KEY}`);
             const response=await resp.json();
             setMatches(await response.matches);
-            console.log(response.matches);
+            //console.log(response.matches);
         }
         catch(error){
             console.log("my error is"+error);
         }
     }
+
 
     useEffect(()=>{
         getMatches();
@@ -36,20 +39,34 @@ function App() {
         <>
         <div className="App">
             <Navbar/>
-            <h1 className="text-center">Welcome to Cricket Live Score App</h1>
-            <Grid container>
+            <Typography variant="h3" className="text-center" style={{marginTop:35}}>Welcome to Cricket Live Score App</Typography>
+            {/* <Grid container>
                 <Grid sm="3"></Grid>
                 <Grid sm="6">
                 {matches.map((match)=>{
                 return(
                     <>
-                    <MyCard key={match.unique_id} match={match} />
+                    {(match.type==="Twenty20")?(
+                        <MyCard key={match.unique_id} match={match}/>
+                    ):(
+                        " "
+                    )}
                     </>
                 )
             })}
                 </Grid>
                 <Grid sm="3"></Grid>
-            </Grid>
+            </Grid> */}
+            <Container>
+        <Grid container>
+            <Grid sm="3"></Grid>
+            <Grid item xs={12} sm="6">
+            {matches.map((match) => (
+              <MyCard match={match}></MyCard>
+            ))}
+          </Grid>
+        </Grid>
+      </Container>
         </div>
         </>
     )
--- src/components/MyCard.jsx
+++ src/components/MyCard.jsx
@@ -4,10 +4,36 @@ import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
-import React from 'react';
+import React,{useState} from 'react';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
 //import { getMatchDetail } from "../api/Api.jsx";
+const API_KEY="SZ5pcaz1Zrdel8jtcTBhEdq9sKz2";
 
 const MyCard = (props) => {
+
+    const [detail, setDatail] = useState({});
+    const [open, setOpen] = useState(false);
+
+    function handleClick(id){
+        getMatchDetails(id)
+        .then((data)=>{
+            console.log("Match Data",data)
+            setDatail(data);
+            handleClickOpen();
+            })
+        .catch((error)=>console.log(error));
+    }
+
+    const getMatchDetails=(id)=>{
+        const url = `https://cricapi.com/api/cricketScore?apikey=${API_KEY}&unique_id=${id}`;
+        return fetch(url)
+        .then((response)=>response.json())
+        .catch((error)=>console.log(error));
+    }
 
     const getMatchCard=()=>{
         return(
@@ -35,7 +61,9 @@ const MyCard = (props) => {
 
                 <CardActions>
                     <Grid container justify="center">
-                        <Button variant="contained" color="primary">
+                        <Button variant="contained" color="primary" onClick={()=>{
+                            handleClick(props.match.unique_id);
+                        }}>
                             Show Details
                         </Button>
                         <Button style={{marginLeft:19}} variant="contained" color="primary">
@@ -48,8 +76,53 @@ const MyCard = (props) => {
             </>
         )
     }
+
+    const handleClickOpen = () => {
+        setOpen(true);
+      };
+
+    const handleClose = () => {
+        setOpen(false);
+      };
+
+    const getDialog=()=>(
+        <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{"Match Detail..."}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            <Typography>{detail.stat}</Typography>
+            <Typography>
+              Match
+              <span style={{ fontStyle: "italic", fontWeight: "bold" }}>
+                {detail.matchStarted ? "Started" : "Still not started"}
+              </span>
+            </Typography>
+            <Typography>
+              Score
+              <span style={{ fontStyle: "italic", fontWeight: "bold" }}>
+                {" "}
+                {detail.score}
+              </span>
+            </Typography>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary" autoFocus>
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
     return (
-        getMatchCard()
+        <>
+        {getMatchCard()}
+        {getDialog()}
+        </>
     )
 }
 
